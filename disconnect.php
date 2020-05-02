3a25108b8c3e1be863f592061c785688cb9c5a5e
Franck Perie <franck.perie24@gmail.com>
2020-05-02 16:32:51 +0200

    [FIX] Bugs CDN Jquery résolus

--- disconnect.php
+++ disconnect.php
@@ -23,8 +23,9 @@ header("Location: index.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="/BurgerCode/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
     <link href="https://fonts.googleapis.com/css2?family=Holtwood+One+SC&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/styles.css">
@@ -43,7 +44,7 @@ header("Location: index.php");
             <h2>Deconnection</h2>
                 <br>
                 <form action="disconnect.php" class="form" method="POST" role="form">
-                   <input type="hidden" name="id" value="<?php echo $id ?>"></input> <!--Récupére la valeur de mon ID du POST dans un input invisible à l'utilisateur.-->
+                   <input type="hidden" name="id" value="id"></input>  <!--Récupére la valeur de mon ID du POST dans un input invisible à l'utilisateur. -->
                     <p class="alert alert-warning">Etes vous sur de vouloir vous déconnecter? ?</p>
                     <div class="form-actions">
                         <button type="submit" class="btn btn-warning"> Oui</button>
